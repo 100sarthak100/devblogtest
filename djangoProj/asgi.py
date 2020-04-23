8370c491831c1396b750507cff408423bcf53cef
Sarthak Naithani <sarthaknaithani127@gmail.com>
2020-04-23 21:53:50 +0530

    changed asgi.py

--- djangoProj/asgi.py
+++ djangoProj/asgi.py
@@ -24,6 +24,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_channels_heroku.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProj.settings")
 django.setup()
 application = get_default_application()
