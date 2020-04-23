f32837b02ee3dac64b564e82e5f9dec5c763c29a
Sarthak Naithani <sarthaknaithani127@gmail.com>
2020-04-23 23:20:06 +0530

    redis_url

--- djangoProj/settings.py
+++ djangoProj/settings.py
@@ -203,7 +203,7 @@ CHANNEL_LAYERS = {
     "default": {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         "CONFIG": {
-            "hosts": [os.environ.get('REDISCLOUD_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
         #"ROUTING": "chat.routing.channel_routing",
     },
