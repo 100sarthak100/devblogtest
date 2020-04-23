c45d13d8edc84236f71178df25dd4019f0b2c0a8
Sarthak Naithani <sarthaknaithani127@gmail.com>
2020-04-23 13:17:57 +0530

    added meta tags and allowed hosts

--- djangoProj/settings.py
+++ djangoProj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 #DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['devblogapp.herokuapp.com']
+ALLOWED_HOSTS = ['devblogapp.herokuapp.com','www.mydevblog.me','mydevblog.me']
 
 
 # Application definition
