5e564d9721b4e7ab3b6860af0c16988e2b023177
Sarthak Naithani <sarthaknaithani127@gmail.com>
2020-04-25 11:47:19 +0530

    changed media url

--- djangoProj/settings.py
+++ djangoProj/settings.py
@@ -212,7 +212,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
+#AWS_DEFAULT_ACL = None
 
 #DEFAULT_FILE_STORAGE = 'djangoProj.s3utils.MediaRootS3BotoStorage'
 #STATICFILES_STORAGE = 'myproyect.s3utils.StaticRootS3BotoStorage'
@@ -224,7 +225,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # 'django.contrib.staticfiles.finders.AppDirectoriesFinder'
 # )
 
-S3_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+S3_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.us-west-2.amazonaws.com/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 #STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
