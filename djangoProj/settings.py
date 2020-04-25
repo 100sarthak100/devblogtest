a3c905626911a4c04c0c984b6380f6aa3b2be232
Sarthak Naithani <sarthaknaithani127@gmail.com>
2020-04-25 11:50:04 +0530

    changed media url2

--- djangoProj/settings.py
+++ djangoProj/settings.py
@@ -212,8 +212,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
-#AWS_DEFAULT_ACL = None
+#AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 
 #DEFAULT_FILE_STORAGE = 'djangoProj.s3utils.MediaRootS3BotoStorage'
 #STATICFILES_STORAGE = 'myproyect.s3utils.StaticRootS3BotoStorage'
