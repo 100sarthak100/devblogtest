6e4dfc11b06d8e5b6c8912b8e2b3d69d2535af5d
Sarthak Naithani <sarthaknaithani127@gmail.com>
2020-06-02 14:18:44 +0530

    added gzip middleware in settings.py

--- djangoProj/settings.py
+++ djangoProj/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
 ]
 
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
