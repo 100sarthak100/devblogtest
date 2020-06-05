5c83442ab96ad6baf6f57ae10bf248e7ad7122d8
Sarthak Naithani <sarthaknaithani127@gmail.com>
2020-06-05 09:08:16 +0530

    changed priority sitemap again

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -10,7 +10,7 @@ class PostSitemap(Sitemap):
         return Post.objects.all()
 
 class StaticViewSitemap(Sitemap):
-    priority = 1.0
+    priority = 0.5
 
     def items(self):
         return ['about','contact','blogHome','blogList','profile','login','logout','register']
