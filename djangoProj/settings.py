1d13af3d6c5c588a291cdbdc8e95018d3cc20a70
Sarthak Naithani <sarthaknaithani127@gmail.com>
2020-10-11 17:05:09 +0530

    Made changed to About.html file

--- djangoProj/settings.py
+++ djangoProj/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0v4%a84s7b%9iupsri-eg=l0$wxe_)gyifcns%1jg=5wixm+o9'
+# SECRET_KEY = '0v4%a84s7b%9iupsri-eg=l0$wxe_)gyifcns%1jg=5wixm+o9'
 
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 #62b7c2cc9fd8b6b5148514d25d2c3fab3c0ee121e7b98b52
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
